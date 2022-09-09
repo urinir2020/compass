7eb67e77b6a2a657fafbada09ed7f034e9a1397f
urinir2020 <112821482+urinir2020@users.noreply.github.com>
2022-09-09 11:34:30 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,26 @@
+let Direction2 = 0
 basic.forever(function () {
-    if (input.compassHeading() >= 316 && input.compassHeading() <= 360) {
-        basic.showString("N")
-    } else if (input.compassHeading() >= 0 && input.compassHeading() <= 45) {
-        basic.showString("N")
-    } else if (input.compassHeading() >= 46 && input.compassHeading() <= 135) {
-        basic.showString("E")
-    } else if (input.compassHeading() >= 136 && input.compassHeading() <= 225) {
-        basic.showString("S")
+    Direction2 = input.compassHeading()
+    if (Direction2 > input.compassHeading() + 10) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
+        SuperBit.enMotors.M1,
+        -255
+        )
+    } else if (Direction2 < input.compassHeading() + 10) {
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        -255,
+        SuperBit.enMotors.M1,
+        255
+        )
     } else {
-        basic.showString("W")
+        SuperBit.MotorRunDual(
+        SuperBit.enMotors.M1,
+        255,
+        SuperBit.enMotors.M1,
+        255
+        )
     }
-})
-basic.forever(function () {
-	
 })
